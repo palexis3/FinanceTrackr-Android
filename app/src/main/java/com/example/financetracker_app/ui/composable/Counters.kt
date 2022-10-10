fc35192377ee11150deea1c914cad0270efa534c
Patrick Alexis <alexispatrick77@gmail.com>
2022-10-09 20:56:40 -0700

    Initialized Android repo

---
+++ app/src/main/java/com/example/financetracker_app/ui/composable/Counters.kt
@@ -0,0 +1,61 @@
+package com.example.financetracker_app.ui.composable
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.financetracker_app.R
+
+//@Composable
+//fun WaterCounter(modifier: Modifier = Modifier) {
+//    Column(modifier = modifier.padding(16.dp)) {
+//        var count by rememberSaveable { mutableStateOf(0) }
+//
+//        if (count > 0) {
+//            var showTask by remember { mutableStateOf(true) }
+//            if (showTask) {
+//                WellnessTaskItem(
+//                    taskName = "Have you taken your 15 minute walk today?"
+//                )
+//            }
+//            Text(text = stringResource(R.string.water_count, count))
+//        }
+//
+//        Row(Modifier.padding(top = 8.dp)) {
+//            Button(onClick = { count++ }, enabled = count < 10) {
+//                Text(stringResource(id = R.string.add_one))
+//            }
+//            Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
+//                Text("Clear water count")
+//            }
+//        }
+//    }
+//}
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text(stringResource(id = R.string.water_count, count))
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count = count, onIncrement = { count++ }, modifier)
+}
---
+++ app/src/main/java/com/example/financetracker_app/ui/composable/WellnessScreen.kt
@@ -0,0 +1,27 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.financetracker_app.repository.WellnessViewModel
+import com.example.financetracker_app.ui.composable.StatefulCounter
+import com.example.financetracker_app.ui.composable.WellnessTaskList
+
+@Preview
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        WellnessTaskList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task -> wellnessViewModel.remove(task) }
+        )
+    }
+}
