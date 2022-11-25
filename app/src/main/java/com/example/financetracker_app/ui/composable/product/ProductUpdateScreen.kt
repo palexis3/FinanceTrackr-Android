5215694a48b43156ecc8e020158d279336e7c9e7
Patrick Alexis <alexispatrick77@gmail.com>
2022-11-25 00:02:00 -0800

    Fix import

--- app/src/main/java/com/example/financetracker_app/ui/composable/product/ProductUpdateScreen.kt
+++ app/src/main/java/com/example/financetracker_app/ui/composable/product/ProductUpdateScreen.kt
@@ -1,10 +1,23 @@
 package com.example.financetracker_app.ui.composable.product
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
