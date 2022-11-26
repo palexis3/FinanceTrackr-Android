f25014cd7056b8614b4d50e3c27a4aa8ac06e2cd
Patrick Alexis <alexispatrick77@gmail.com>
2022-11-26 13:23:54 -0800

    Migrated common UIs to material3

--- app/src/main/java/com/example/financetracker_app/ui/composable/common/EmittableTextField.kt
+++ app/src/main/java/com/example/financetracker_app/ui/composable/common/EmittableTextField.kt
@@ -3,13 +3,15 @@ package com.example.financetracker_app.ui.composable.common
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.financetracker_app.helper.InputData
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun <T> EmittableTextField(
     inputData: InputData<T>,
